060271558f4c4cd4840666116e7b92791c56aef5
Aravinthan N <aravinthan.n@igosolutions.eu>
2026-02-06 11:16:46 +0530

    n

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
 import React from 'react';
-import App from './App.jsx'
+import App from './App'
 import './index.css';
-import NewTab from './pages/newtab.jsx';
+import NewTab from './pages/newtab';
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
 <StrictMode>
